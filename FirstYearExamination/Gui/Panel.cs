d6c968761d5fb5159a12f57311fbc367c2c2cc0a
Casper Seiden <Seidelin2@hotmail.com>
2020-06-09 12:42:56 +0200

    Class Diagram Added

--- FirstYearExamination/Gui/Panel.cs
+++ FirstYearExamination/Gui/Panel.cs
@@ -8,23 +8,4 @@ using System.Threading.Tasks;
 
 namespace FirstYearExamination.Gui
 {
-    public class Panel
-    {
-        public Vector2 Position { get; set; }
-        public Vector2 Dimensions { get; set; }
-        public Color Color { get; set; }
-
-        private Texture2D texture;
-
-        public void LoadContent(GraphicsDevice device)
-        {
-            texture = Helper.CreateTexture(device, (int)Dimensions.X, (int)Dimensions.Y, (x) => Color);
-        }
-
-        public void Draw(SpriteBatch batch)
-        {
-            batch.Draw(texture, Position, Color);
-        }
-
-    }
 }
